6a0eff5c96921b0e9800420e408d9fa45e497715
kuked <kedoku@gmail.com>
2021-08-24 17:43:35 +0900

    [WIP]Calls and Functions
    * Function Objects
    * Compiling to Function Objects
    * Call Frames

--- object.h
+++ object.h
@@ -7,11 +7,15 @@
 
 #define OBJ_TYPE(value)    (AS_OBJ(value)->type)
 
+#define IS_FUNCTION(value) isObjType(value, OBJ_FUNCTION)
 #define IS_STRING(value) isObjType(value, OBJ_STRING)
+
+#define AS_FUNCTION(value) ((ObjFunction *)AS_OBJ(value))
 #define AS_STRING(value)  ((ObjString *)AS_OBJ(value))
 #define AS_CSTRING(value) (((ObjString *)AS_OBJ(value))->chars)
 
 typedef enum {
+    OBJ_FUNCTION,
     OBJ_STRING,
 } ObjType;
 
@@ -34,6 +38,7 @@ struct ObjString {
     uint32_t hash;
 };
 
+ObjFunction *newFunction();
 ObjString *takeString(char *chars, int length);
 ObjString *copyString(const char *chars, int length);
 void printObject(Value value);
