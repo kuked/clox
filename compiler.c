4d45ea8d570132758dbe5d564bf42591fe5fea9d
kuked <kedoku@gmail.com>
2021-09-06 18:56:28 +0900

    [WIP] Upvalues 😭

--- compiler.c
+++ compiler.c
@@ -48,6 +48,11 @@ typedef struct {
     int depth;
 } Local;
 
+typedef struct {
+    uint8_t index;
+    bool isLocal;
+} Upvalue;
+
 typedef enum {
   TYPE_FUNCTION,
   TYPE_SCRIPT
@@ -60,6 +65,7 @@ typedef struct Compiler {
 
     Local locals[UINT8_COUNT];
     int localCount;
+    Upvalue upvalues[UINT8_COUNT];
     int scopeDepth;
 } Compiler;
 
@@ -276,8 +282,23 @@ static int resolveLocal(Compiler *compiler, Token *name) {
 }
 
 static int addUpvalue(Compiler *compiler, uint8_t index, bool isLocal) {
-    // TODO
-    return 0;
+    int upvalueCount = compiler->function->upvalueCount;
+
+    for (int i = 0; i < upvalueCount; i++) {
+        Upvalue *upvalue = &compiler->upvalues[i];
+        if (upvalue->index == index && upvalue->isLocal == isLocal) {
+            return i;
+        }
+    }
+
+    if (upvalueCount == UINT8_COUNT) {
+        error("Too many closure variables in function.");
+        return 0;
+    }
+
+    compiler->upvalues[upvalueCount].isLocal = isLocal;
+    compiler->upvalues[upvalueCount].index = index;
+    return compiler->function->upvalueCount++;
 }
 
 static int resolveUpvalue(Compiler *compiler, Token *name) {
@@ -286,6 +307,11 @@ static int resolveUpvalue(Compiler *compiler, Token *name) {
     int local = resolveLocal(compiler->enclosing, name);
     if (local != -1) {
         return addUpvalue(compiler, (uint8_t)local, true);
+    }
+
+    int upvalue = resolveUpvalue(compiler->enclosing, name);
+    if (upvalue != -1) {
+        return addUpvalue(compiler, (uint8_t)upvalue, false);
     }
 
     return -1;
@@ -412,6 +438,11 @@ static void function(FunctionType type) {
 
     ObjFunction *function = endCompiler();
     emitBytes(OP_CLOSURE, makeConstant(OBJ_VAL(function)));
+
+    for (int i = 0; i < function->upvalueCount; i++) {
+        emitByte(compiler.upvalues[i].isLocal ? 1 : 0);
+        emitByte(compiler.upvalues[i].index);
+    }
 }
 
 static void funDeclaration() {
